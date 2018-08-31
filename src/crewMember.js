dcb718e1257fc9f27ba8ddf539e1a1723744369b
github username <github email address>
2018-08-31 15:15:16 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,33 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip="Looking for a Rig", specialAbility=null) {
+    this.position = position
+    this.currentShip = currentShip
+    this.specialAbility = specialAbility
+    // this.currentShip.crew.push(this)
+  }
+  
+    engageWarpDrive(){
+      if (this.position === "Pilot" && this.currentShip !== "Looking for a Rig") {
+        console.log("Bam")
+      } else {
+        return "had no effect"
+      }
+    }
+    
+    setsInvisibility(){
+      if (this.position === "Defender" && this.currentShip !== "Looking for a Rig") {
+        console.log("Woosh")
+        this.currentShip.cloaked = true
+    } else {
+      return "had no effect"
+    }
+  }
+    
+    chargePhasers(){
+      if (this.position === "Gunner" && this.currentShip !== "Looking for a Rig") {
+        console.log("Zing")
+    } else {
+      return "had no effect"
+    }
+  }  
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,25 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crewArray=[], phasers, shields) {
+    this.name = name
+    this.crew = crewArray
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    this.docked = this.crew.length < 1
+    // for each crew member, set currentShip = this
+    this.updateCrewMembers(crewArray)
+  }
+  
+  updateCrewMembers(crewArray) {
+    for (let crewMember of crewArray) {
+      crewMember.currentShip = this
+    }
+  }
+  
+  // onboard(crewMember) {
+  //   this.crew.push(crewMember)
+  //   crewMember.currentShip = this
+  // }  
+}
